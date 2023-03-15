c589ee3f67e8c709070efd4afd97d779604fafda
David Dinh <dndinh7@gmail.com>
2023-03-15 18:58:38 -0400

    seem to have a working phong model after changing parameters and lightpos in eye coordinates

--- shaders/phong-vertex.vs
+++ shaders/phong-vertex.vs
@@ -29,13 +29,13 @@ struct MaterialProp {
 
 uniform MaterialProp Material;
 
-vec3 phong(vec4 p_eye, vec3 n_eye, vec4 lightPos_eye) {
+vec3 phong(vec4 p_eye, vec3 n_eye) {
    // vector to the light source
    vec3 s;
    if (Light.Pos.w == 0.0f) // directional light source
-      s= normalize(vec3(lightPos_eye));
+      s= normalize(vec3(Light.Pos));
    else // positional light source
-      s= normalize(vec3(lightPos_eye - p_eye));
+      s= normalize(vec3(Light.Pos - p_eye));
    
    vec3 v= normalize(-vec3(p_eye));
 
@@ -55,9 +55,8 @@ void main()
    // get the normal and vertex position to eye coordinates
    vec3 n_eye= normalize(NormalMatrix * vNormals);
    vec4 p_eye= ModelViewMatrix * vec4(vPos, 1.0);
-   vec4 lightPos_eye= ModelViewMatrix * Light.Pos;
    
-   Intensity= phong(p_eye, n_eye, lightPos_eye);
+   Intensity= phong(p_eye, n_eye);
 
    gl_Position = MVP * vec4(vPos, 1.0);
 }
--- src/mesh-viewer.cpp
+++ src/mesh-viewer.cpp
@@ -133,17 +133,24 @@ public:
         /*vec3 lightColor= vec3(sin(elapsedTime() * 2.0f), sin(elapsedTime()*0.7f), sin(elapsedTime()*1.3f));
         vec3 Ld= lightColor * vec3(0.5f);
         vec3 La= Ld * vec3(0.2f);*/
-        vec3 La= vec3(0.2f);
-        vec3 Ld= vec3(0.5f);
+        vec3 La= vec3(0.2f); // light ambience 
+        vec3 Ld= vec3(0.5f); // light diffusion
+        vec3 Ls= vec3(1.0f); // light specular
+        vec3 lightPosition= vec3(10.0f, 2.5f, 5.0f);
+        renderer.translate(lightPosition); // convert to eye pos
         
         renderer.setUniform("Light.La", La);
         renderer.setUniform("Light.Ld", Ld);
-        renderer.setUniform("Light.Ls", vec3(1.0f, 1.0f, 1.0f));
-        renderer.setUniform("Light.Pos", vec4(10.0f, 2.5f, 5.0f, 1.0f));
+        renderer.setUniform("Light.Ls", Ls);
+        renderer.setUniform("Light.Pos", vec4(lightPosition, 1.0f));
+
+        vec3 Ka= vec3(0.21f, 0.31f, 1.0f);
+        vec3 Kd= vec3(0.21f, 0.31f, 1.0f);
+        vec3 Ks= vec3(0.5f);
         
-        renderer.setUniform("Material.Ka", vec3(0.21f, 0.31f, 1.0f));
-        renderer.setUniform("Material.Kd", vec3(0.21f, 0.31f, 1.0f));
-        renderer.setUniform("Material.Ks", vec3(0.5f, 0.5f, 0.5f));
+        renderer.setUniform("Material.Ka", Ka);
+        renderer.setUniform("Material.Kd", Kd);
+        renderer.setUniform("Material.Ks", Ks);
         renderer.setUniform("Material.alpha", 32.0f);
       }
 
